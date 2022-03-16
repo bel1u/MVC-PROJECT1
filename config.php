72665fa66c204afcc4df42e4db09a25e2119351b
Is41-belyysa <bel1u666ss@mail.ru>
2022-03-16 13:52:07 +0500

    11

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is341_1_bel1u';
+const DB_PASSWORD = 'qwerty123';
+const DB_DATABASE = 'is341_1_test';
 const DB_PORT = '3306'; # Default 3306
